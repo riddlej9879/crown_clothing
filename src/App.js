d50d17947b90a273ae39241a020f70a3cc316303
Josh Riddle <riddlej9879@yahoo.com>
2021-06-21 16:19:22 -0500

    Repo creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+// This is a new line
 function App() {
   return (
     <div className="App">
